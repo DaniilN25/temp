d40dd404dde98671c966136ee9805e9e51d9b718
NesterovDaniil <daniil2505200@gmail.com>
2023-03-13 16:04:59 +0700

    second prractic

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral']
+    }
+})
